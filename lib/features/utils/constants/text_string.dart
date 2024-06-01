ed572a1d479bc0b4b55d55ee5fc43b361835e13c
the-raccoondog <dylan1406lewis@gmail.com>
2024-06-01 14:34:11 +0530

    text strings

---
+++ lib/features/utils/constants/text_string.dart
@@ -0,0 +1,16 @@
+class TTexts {
+  //Global texts
+
+  //Onboarding texts
+  static const onBoardingTitle1="Choose your Product";
+  static const onBoardingTitle2="Select Payment Method";
+  static const onBoardingTitle3="Deliver at your Door Step";
+
+  static const onBoardingSubTitle1="Welcome to a World of Limitless Choices - Your Perfect Product Awaits!";
+  static const onBoardingSubTitle2="For Seamless Transactions, Choose Your Payment Path - Your Convience, Our Priority";
+  static const onBoardingSubTitle3="From Our Doorstep to Yours - Swift, Secure, and Contactless Delivery";
+
+  //Home
+  static const homeAppbarTitle="Good day for shopping";
+  static const homeAppbarSubTitle="Aary Jadhav";
+}
