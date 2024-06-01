038d0df9947d04e29824587ea595e8879ec21a0d
the-raccoondog <dylan1406lewis@gmail.com>
2024-06-01 14:28:07 +0530

    logos

---
+++ lib/features/utils/constants/image_strings.dart
@@ -0,0 +1,11 @@
+//This class contaion all the App images in string formats
+
+class TImages{
+  //App logos
+  static const String darkAppLogo="assets/logos/store-darklogo.png";
+  static const String lightAppLogo="assets/logos/store-lightlogo.png";
+
+  //Social logos
+  static const String google="assets/logos/google.png";
+  static const String facebook="assets/logos/facebook.png";
+}
