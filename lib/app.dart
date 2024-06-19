190f250c004a3898fe110e6284ef46165bf022f7
the-raccoondog <dylan1406lewis@gmail.com>
2024-06-19 20:35:54 +0530

    1

--- lib/app.dart
+++ lib/app.dart
@@ -7,14 +7,13 @@ import 'package:get/get_navigation/src/root/get_material_app.dart';
 class App extends StatelessWidget {
   const App({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       themeMode: ThemeMode.system,
       theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
-      home:OnBoardingScreen(),
+      home: OnBoardingScreen(),
     );
   }
 }
